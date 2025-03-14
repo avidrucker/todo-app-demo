d9c5b1db3b8d5a200568a49136308fb2a2587ad4
Avi Drucker <avidrucker@users.noreply.github.com>
2025-03-14 11:57:13 -0400

    add list pretty print, clear screen ability

--- todo.py
+++ todo.py
@@ -1,3 +1,14 @@
+# idea: pretty print the list (1 item on each line)
+# idea: clear the screen after we print the list
+# idea: delete item from the list
+# idea: edit an item in the list
+# idea: add numbers to the items when I print them out
+# idea: print in color (q: does it need a special library?)
+
+import os # import the 'os' library
+
+# clear the terminal window before everything else
+os.system('cls' if os.name == 'nt' else 'clear')
 print("Welcome to my to-do list app!") # greet the user
 my_list = [] # make a list to save items
 while True:
@@ -8,4 +19,6 @@ while True:
         break
     else:
         my_list.append(new_item) # add new item to list
-        print(my_list)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for item in my_list:
+            print(item)
