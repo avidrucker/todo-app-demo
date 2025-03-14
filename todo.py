412e91bca7b80ec6080ed655a460efe6ff8a5fba
Avi Drucker <avidrucker@users.noreply.github.com>
2025-03-14 11:42:03 -0400

    add quit option, goodbye message

--- todo.py
+++ todo.py
@@ -2,6 +2,10 @@ print("Welcome to my to-do list app!") # greet the user
 my_list = [] # make a list to save items
 while True:
     # save the newly inputted item
-    new_item = input("Please enter a new item: ")
-    my_list.append(new_item) # add new item to list
-    print(my_list)
+    new_item = input("Please enter a new item or 'q' to quit: ")
+    if new_item == 'q':
+        print("Goodbye!")
+        break
+    else:
+        my_list.append(new_item) # add new item to list
+        print(my_list)
